121c595e27cef4229cf2e2822617edf06074c893
pawel-bogdan <72096306+pawel-bogdan@users.noreply.github.com>
2021-07-15 22:47:25 +0200

    Add files via upload

--- src/GUIManager.java
+++ src/GUIManager.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
